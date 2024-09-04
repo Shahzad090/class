013bb76812d5efe8b6b924fc2a9f691efc7d2517
Shahzad090 <153703186+Shahzad090@users.noreply.github.com>
2024-09-04 20:41:22 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+// let a = {
+//     id: 1,
+//     name: "fgghgh",
+// }
+
+// let b = {...a};
+// b.inst = "SMIT"
+// console.log(b);
+
+let a = ["basit", "mubashir", "raza", "hamza"]
+let [firstName, secondName, thirdName, forthName] = a;
+console.log(firstName);
